ddc5751a395ee370765c94d31a7099325ea55890
Danniel Cristie <dany14012@gmail.com>
2022-09-27 11:25:12 -0300

    test: should increase date with one year

---
+++ src/tests/utils/get-future-date.spec.ts
@@ -0,0 +1,8 @@
+import { getFutureDate } from './get-future-date';
+import { expect, test } from "vitest";
+
+test('increase date with one year', () => {
+    const year = new Date().getFullYear()
+
+    expect(getFutureDate(`${year}-08-10`).getFullYear()).toEqual(2023)
+});
