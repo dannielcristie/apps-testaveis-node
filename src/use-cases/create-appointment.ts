fd2d9432db6f57fd6701d3df3f155bfbd784fb52
Danniel Cristie <dany14012@gmail.com>
2022-09-27 13:06:57 -0300

    chore:  add appoitement repository dependencies in use case

--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -1,3 +1,4 @@
+import { AppointmentsRepository } from './../repositories/appointment-repository';
 import { Appointment } from './../entities/appointment';
 
 interface CreateAppointmentRequest {
@@ -9,16 +10,32 @@ interface CreateAppointmentRequest {
 type CreateAppointmentResponse = Appointment
 
 export class CreateAppointement {
+  constructor(
+    private appointmentsRepository: AppointmentsRepository
+  ) { }
+
   async execute ({
     customer,
     startsAt,
     endsAt
   }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlappingAppointment = await this.appointmentsRepository.findOverlappingAppointment(
+      startsAt,
+      endsAt
+    )
+    
+    if(overlappingAppointment){
+      throw new Error('Another appointement overlaps this appointment dates')
+    }
+
+
     const appointment = new Appointment({
       customer,
       startsAt,
       endsAt
     })
+
+    await this.appointmentsRepository.create(appointment)
 
     return appointment
   }
